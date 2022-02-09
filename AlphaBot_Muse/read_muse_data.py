f6e92cf4a5682a2155f0eaa00b567ee03dac5920
RamondettiMarco <93180431+RamondettiMarco@users.noreply.github.com>
2022-02-09 11:46:02 +0100

    Aggiunta cartella completa dei codici del Muse2

---
+++ AlphaBot_Muse/read_muse_data.py
@@ -0,0 +1,127 @@
+"""
+read muse data
+PCTO project of ITIS MARIO DELPOZZO CUNEO
+"""
+
+import numpy as np 
+from pylsl import StreamInlet, resolve_byprop
+import utils
+import threading, queue
+import time
+from muselsl import stream, list_muses, view
+
+#muselsl stream --ppg --acc --gyro
+
+class Band:
+    Delta = 0
+    Theta = 1
+    Alpha = 2
+    Beta = 3
+
+""" EXPERIMENTAL PARAMETERS """
+BUFFER_LENGTH = 5
+EPOCH_LENGTH = 1
+OVERLAP_LENGTH = 0.8
+SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
+INDEX_CHANNEL = [0]
+
+if __name__ == "__main__":
+
+    """ 1. CONNECT TO EEG STREAM """
+
+    # Search and active LSL streams
+    print('Looking for an EEG stream...')
+    streams = resolve_byprop('type', 'EEG', timeout=2)
+    #print(streams)
+    """muses = list_muses()
+    print(muses)
+    stream(muses[0]['address'], ppg_enabled=True, acc_enabled=True, gyro_enabled=True)
+    print(streams)"""
+        
+    if len(streams) == 0:
+        raise RuntimeError('Can\'t find EEG stream.')
+    print("Start acquiring data")
+    inlet = StreamInlet(streams[0], max_chunklen=12)
+    eeg_time_correction = inlet.time_correction()
+    info = inlet.info()
+    description = info.desc()
+    fs = int(info.nominal_srate())
+
+    """ 2. INITIALIZE BUFFERS """
+    n_win_test = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) / SHIFT_LENGTH + 1))
+    band_buffer = np.zeros((n_win_test, 4))
+        
+    """ 3. GET DATA """
+    try:
+        while True:
+                
+            """prova per concentrazione"""
+            #band_beta = 'STOP'
+                
+            #time.sleep(5)
+            def muse():
+                eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
+                filter_state = None  # for use with the notch filter
+                q = queue.Queue()
+                cnt = 0
+                c = 0 
+                command = 'ESCI'
+                while (cnt < 5):
+                    """ 3.1 ACQUIRE DATA """
+                    eeg_data, timestamp = inlet.pull_chunk(timeout=1, max_samples=int(SHIFT_LENGTH * fs))
+                    ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
+                    eeg_buffer, filter_state = utils.update_buffer(eeg_buffer, ch_data, notch=True, filter_state=filter_state)
+
+                    """ 3.2 COMPUTE BAND POWERS """
+                    data_epoch = utils.get_last_data(eeg_buffer, EPOCH_LENGTH * fs)
+                    band_powers = utils.compute_band_powers(data_epoch, fs)
+                    #print (band_powers)
+                    band_beta = utils.compute_beta(data_epoch, fs)
+                        
+                    if(band_beta == 'W'):
+                        c += 1
+                    
+                    q.put(band_beta)
+                    cnt += 1
+                    time.sleep(0.5)
+                    
+                if(c >= 3):
+                    command = 'W'
+                    print(command)
+                else:
+                    command = 'ESCI'
+                    print(command)
+                    
+                time.sleep(1)    
+                c = 0
+                        
+                def worker():
+                    while True:
+                        band_beta = q.get()
+                        q.task_done()
+
+                # turn-on the worker thread
+                        
+                threading.Thread(target=worker, daemon=True).start()
+                time.sleep(5)
+                cnt = 0 
+                #cnt -= 1
+                    
+                #print(f'Working on {item}')
+                #print(f'Finished {item}')
+                #print('All task requests sent\n', end='')
+
+                # block until all tasks are done
+                q.join()
+                #print('All work completed')
+                    
+                #band_beta = utils.compute_beta(data_epoch, fs)
+                #print(band_beta)
+                    
+                #band_alpha = utils.compute_alpha(data_epoch, fs)
+                return command
+            muse()
+
+    except KeyboardInterrupt:
+        print('Closing!')
+
---
+++ AlphaBot_Muse/startMuseStream_Finale.py
@@ -0,0 +1,109 @@
+"""
+Starting a Stream
+
+This example shows how to search for available Muses and
+create a new stream
+"""
+from muselsl import stream, list_muses
+from pylsl import StreamInlet, resolve_byprop
+import utils
+import time
+import numpy as np
+
+if __name__ == "__main__":
+    
+    BUFFER_LENGTH = 5
+
+# Length of the epochs used to compute the FFT (in seconds)
+    EPOCH_LENGTH = 1
+
+# Amount of overlap between two consecutive epochs (in seconds)
+    OVERLAP_LENGTH = 0.8
+
+# Amount to 'shift' the start of each next consecutive epoch
+    SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
+
+# Index of the channel(s) (electrodes) to be used
+# 0 = left ear, 1 = left forehead, 2 = right forehead, 3 = right ear
+    INDEX_CHANNEL = [0]
+
+    stream("00:55:da:b5:49:3e", ppg_enabled=True, acc_enabled=True, gyro_enabled=True)
+    #print(stream("00:55:da:b5:49:3e"))
+    streams_Gyro = resolve_byprop('type', 'Gyroscope', timeout=2)
+    #creare un'altra stream per EEG
+    streams_EEG = resolve_byprop('type', 'EEG', timeout=2)
+    #print(streams_Gyro)
+    #print(streams_EEG)
+    #secondo inlet per EEG
+    inlet_Gyro = StreamInlet(streams_Gyro[0], max_chunklen=12)
+    info_Gyro = inlet_Gyro.info()
+    #print(info.desc())
+    
+    inlet_EEG = StreamInlet(streams_EEG[0], max_chunklen=12)
+    info_EEG  = inlet_EEG.info()  
+    
+    fs_Gyro = int(info_Gyro.nominal_srate())
+    #fs2 per EEG
+    fs_EEG = int(info_EEG.nominal_srate())
+    #print(fs)
+    
+    def museDxSx():
+        """ 3.1 ACQUIRE DATA """
+        # Obtain EEG data from the LSL stream
+        gyro_data, timestamp = inlet_Gyro.pull_chunk(
+        timeout=1, max_samples=int(SHIFT_LENGTH * fs_Gyro))
+        #print(eeg_data[-1])
+        Theta = 0.5*(gyro_data[-1][2] + gyro_data[-2][2]) * 1/fs_Gyro #velocita in questo istante
+        if(Theta > 0.1):
+            comando = 'A'
+            print("Gyroscopoe: ", comando)
+        elif(Theta < -0.1):
+            comando = 'D'
+            print("Gyroscopoe: ", comando)
+        else:
+            comando = 'W'
+            print("Gyroscopoe: ", comando)
+                    
+        #print(Theta)
+        #print(timestamp)
+        return comando
+            
+    def museConcentrazione():
+        eeg_buffer = np.zeros((int(fs_EEG * BUFFER_LENGTH), 1))
+        filter_state = None  # for use with the notch filter
+        EEG_data, timestamp = inlet_EEG.pull_chunk(
+            timeout=1, max_samples=int(SHIFT_LENGTH * fs_EEG))
+        ch_data = np.array(EEG_data)[:, INDEX_CHANNEL]
+        eeg_buffer, filter_state = utils.update_buffer(eeg_buffer, ch_data, notch=True, filter_state=filter_state)
+
+        """ 3.2 COMPUTE BAND POWERS """
+        data_epoch = utils.get_last_data(eeg_buffer, EPOCH_LENGTH * fs_EEG)
+        band_powers = utils.compute_band_powers(data_epoch, fs_EEG)
+        #print (band_powers)
+        band_beta = utils.compute_beta(data_epoch, fs_EEG)
+                
+        #print(EEG_data[-1])
+        #Beta = 0.5*(EEG_data[-1][2] + EEG_data[-2][2]) * 1/fs_EEG #velocita in questo istante
+        #print(Beta)
+        #time.sleep(1)
+        """if(Beta > 8):
+            print('concentrato')
+        else:
+            print('non concentrato')"""
+        return band_beta
+    
+    while True: 
+            museDxSx()
+            
+            print("----------------------------------------------------------------")
+                
+            c = museConcentrazione()
+            print("concentrazione: ", c)
+    
+    """if not muses:
+        print('No Muses found')
+    else:
+        stream("00:55:da:b5:49:3e", ppg_enabled=True, acc_enabled=True, gyro_enabled=True)"""
+
+        # Note: Streaming is synchronous, so code here will not execute until the stream has been closed
+    print('Stream has ended')
