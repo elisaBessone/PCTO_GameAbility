03c4ac9ff1771e1c2504f2b1e73ef4e62cff32c1
Elisa Bessone <93180470+elisaBessone@users.noreply.github.com>
2022-10-21 08:14:21 +0200

    Update clientCamera.py

--- StreamTelecamera/clientCamera.py
+++ StreamTelecamera/clientCamera.py
@@ -7,36 +7,35 @@ import picamera
 
 client_socket = socket.socket()
 
-client_socket.connect(('192.168.0.119', 8000))  # aggiungere IP
+client_socket.connect(('192.168.0.119', 8000))  # add IP
 
-# creazione di un oggetto simile a un file di connessione
+# creation of an object similar to a connection file
 connection = client_socket.makefile('wb')
 try:
     camera = picamera.PiCamera()
     camera.vflip = True
     camera.resolution = (500, 480)
 
-    #avvio anteprima per scaldare la telecamera
     camera.start_preview()
     time.sleep(2)
 
-    # creazione del flusso per contenere temporaneamente i dati dell'immagine
+    
     start = time.time()
     stream = io.BytesIO()
     for foo in camera.capture_continuous(stream, 'jpeg'):
-        # scrittura della lunghezza della cattura del flusso per assicurarsi che venga attivato
+        
         connection.write(struct.pack('<L', stream.tell()))
         connection.flush()
-        # riavvolgo i dati e li invio al server
+        
+        # data sent to the server
         stream.seek(0)
         connection.write(stream.read())
-        # condizione di uscita
+        
         if time.time() - start > 60:
             break
-        # reset dello stream per la prossima cattura video
+        # reset stream
         stream.seek(0)
         stream.truncate()
-    # scrittura della fine della connessione
     connection.write(struct.pack('<L', 0))
 finally:
     connection.close()
