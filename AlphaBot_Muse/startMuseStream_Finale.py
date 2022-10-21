d5e7c67e73c8994fbb099f9ab130ea3c8d02c456
RamondettiMarco <93180431+RamondettiMarco@users.noreply.github.com>
2022-10-21 08:21:47 +0200

    Update startMuseStream_Finale.py

--- AlphaBot_Muse/startMuseStream_Finale.py
+++ AlphaBot_Muse/startMuseStream_Finale.py
@@ -29,12 +29,12 @@ if __name__ == "__main__":
 
     stream("00:55:da:b5:49:3e", ppg_enabled=True, acc_enabled=True, gyro_enabled=True) #"00:55:da:b5:49:3e" = MAC ADDRESS MUSE 2
     #print(stream("00:55:da:b5:49:3e")) 
-    streams_Gyro = resolve_byprop('type', 'Gyroscope', timeout=2) #fa partire il giroscopio
-    #creare un'altra stream per EEG
-    streams_EEG = resolve_byprop('type', 'EEG', timeout=2) #fa partire i segnali EEG, che servono per trovare la concentrazione
+    streams_Gyro = resolve_byprop('type', 'Gyroscope', timeout=2) #starts the gyroscope
+    #create another stream for EEG
+    streams_EEG = resolve_byprop('type', 'EEG', timeout=2) #it triggers the EEG signals, which are used to find concentration
     #print(streams_Gyro)
     #print(streams_EEG)
-    #secondo inlet per EEG
+    #second inlet for EEG
     inlet_Gyro = StreamInlet(streams_Gyro[0], max_chunklen=12) 
     info_Gyro = inlet_Gyro.info()
     #print(info.desc())
@@ -42,9 +42,9 @@ if __name__ == "__main__":
     inlet_EEG = StreamInlet(streams_EEG[0], max_chunklen=12)
     info_EEG  = inlet_EEG.info()  
     
-    fs_Gyro = int(info_Gyro.nominal_srate()) #frequenza del giroscopio
+    fs_Gyro = int(info_Gyro.nominal_srate()) #gyro frequency
     #fs2 per EEG
-    fs_EEG = int(info_EEG.nominal_srate()) #frequenza segnali EEG
+    fs_EEG = int(info_EEG.nominal_srate()) #frequency EEG signals
     #print(fs)
     
     def museDxSx():
@@ -53,7 +53,7 @@ if __name__ == "__main__":
         gyro_data, timestamp = inlet_Gyro.pull_chunk(
         timeout=1, max_samples=int(SHIFT_LENGTH * fs_Gyro))
         #print(eeg_data[-1])
-        Theta = 0.5*(gyro_data[-1][2] + gyro_data[-2][2]) * 1/fs_Gyro #velocita in questo istante, media degli ultimi 2 valori, per giroscopio
+        Theta = 0.5*(gyro_data[-1][2] + gyro_data[-2][2]) * 1/fs_Gyro #speed in this instant, average of the last 2 values, per gyroscope
         if(Theta > 0.1): #va a sinistra
             comando = 'A'
             print("Gyroscopoe: ", comando)
@@ -66,9 +66,9 @@ if __name__ == "__main__":
                     
         #print(Theta)
         #print(timestamp)
-        return comando #il comando che entrerà nell'alphabot
+        return comando #the command that will enter the alphabot
             
-    def museConcentrazione(): #restituisce il comando in entrata all'alphabot per la concentrazione
+    def museConcentrazione(): #returns the incoming command to the alphabot for concentration
         eeg_buffer = np.zeros((int(fs_EEG * BUFFER_LENGTH), 1))
         filter_state = None  # for use with the notch filter
         EEG_data, timestamp = inlet_EEG.pull_chunk(
@@ -80,18 +80,18 @@ if __name__ == "__main__":
         data_epoch = utils.get_last_data(eeg_buffer, EPOCH_LENGTH * fs_EEG)
         band_powers = utils.compute_band_powers(data_epoch, fs_EEG) #band_powers(raggi alpha, beta, theta, delta) cioè tutti gli EEG
         #print (band_powers)
-        band_beta = utils.compute_beta(data_epoch, fs_EEG) #compute_beta funzione per il calcolo dei raggi beta(concentrazione)
+        band_beta = utils.compute_beta(data_epoch, fs_EEG) #compute_beta function for calculating beta rays (concentration)
         
-        #secondo metodo         
+        #a second method         
         #print(EEG_data[-1]) #raggi beta
-        #Beta = 0.5*(EEG_data[-1][2] + EEG_data[-2][2]) * 1/fs_EEG #velocita in questo istante
+        #Beta = 0.5*(EEG_data[-1][2] + EEG_data[-2][2]) * 1/fs_EEG #speed in this instant
         #print(Beta)
         #time.sleep(1)
         """if(Beta > 8):
             print('concentrato')
         else:
             print('non concentrato')"""
-        return band_beta #comando (W / ESCI) per l'alphabot, se concentrato W quindi va avanti, altrimenti ESCI, sta fermo
+        return band_beta #command (W / ESCI) for the alphabot, if concentrated W then go ahead, otherwise EXIT, stand still
     
     while True: 
             museDxSx()
